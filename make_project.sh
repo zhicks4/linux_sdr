34db2e86c9dc5c50f4710d440d8ce480cb0c10b3
Doug Wenstrand <Douglas.Wenstrand@jhuapl.edu>
2022-07-03 16:51:26 -0400

    added linux test prog and script to make .bit.bin file

--- make_project.sh
+++ make_project.sh
@@ -1,3 +1,7 @@
+# the first line constructs the vivado project, but doesn't synthesize or place+route
 vivado -nolog -nojournal -mode batch -source ./tcl/make_project.tcl
+# this next command runs the TCL script that makes the bitfile in vivado and exports the XSA
 vivado -nolog -nojournal -mode batch -source ./tcl/impl.tcl
-xsct ./tcl/make_sw.tcl
+# the next line makes the vitis workspace and builds the app thru boot.bin stage
+# commenting out the Vitis section, since we will test this periph in Linux!
+#xsct ./tcl/make_sw.tcl
---
+++ makebitbin.sh
@@ -0,0 +1,3 @@
+# copy the bitfile to this directory first
+bootgen -image doug.bif -arch zynq -process_bitstream bin -w on
+
