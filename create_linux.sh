59bf6964635bae00a71198b7135cfc525ab4282a
Doug Wenstrand <douglas.wenstrand@jhuapl.edu>
2022-09-12 11:14:38 -0400

    changing ip address, adding python packages

--- create_linux.sh
+++ create_linux.sh
@@ -1,6 +1,8 @@
 PROJECT_DIR=zybo_linux
 HDL_XSA_DIR=./vivado/radio_periph_lab.xsa
 ROOTFS_CONFIG_FILE=rootfs_config
+CONFIG_FILE=config
+
 # create a project
 petalinux-create -t project -n $PROJECT_DIR --template zynq
 
@@ -11,6 +13,7 @@ petalinux-config --get-hw-description $HDL_XSA_DIR --silentconfig -p $PROJECT_DI
 # is a modified version that adds a couple of features.  We will just make a link
 # so our config file can stay at the base level of the repository
 # if you would rather select packages manually, you can be led through the menus
+cp $CONFIG_FILE $PROJECT_DIR/project-spec/configs/config
 petalinux-config -p $PROJECT_DIR --silentconfig
 cp $ROOTFS_CONFIG_FILE $PROJECT_DIR/project-spec/configs/rootfs_config
 petalinux-config -p $PROJECT_DIR -c rootfs --silentconfig
